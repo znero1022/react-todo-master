349236e43b850cfc351060b21e2df786c3aafe6d
KimDayeong <dyyukim@gmail.com>
2024-10-21 11:48:02 +0900

    style: Tailwind 사용자 정의 텍스트 유틸리티 클래스 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,123 @@
 export default {
   content: ["./index.html", "./src/**/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Pretendard", "sans-serif"],
+      },
+      fontSize: {
+        L: "24px",
+        M: "16px",
+        R: "14px",
+        S: "12px",
+      },
+      fontWeight: {
+        Bold: "700",
+        Medium: "500",
+      },
+      grayscalelor: {
+        50: "#EFEFEF",
+        100: "#E4EBF3",
+        200: "#D1D6DA",
+        300: "#949494",
+        400: "#43434A",
+        500: "#1C1C1C",
+      },
+      orange: {
+        100: "#DF5D50",
+        500: "#C84331",
+      },
+      opacity: {
+        80: "0.8", // opacity-80
+      },
+      borderRadius: {
+        radiusuFull: "999px",
+        radiusu100: "8px",
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities, theme }) {
+      const textUtils = {
+        ".BoldL-none": {
+          fontSize: theme("fontSize.L"),
+          fontWeight: theme("fontWeight.bold"),
+          textDecoration: "none",
+        },
+        ".BoldM-none": {
+          fontSize: theme("fontSize.M"),
+          fontWeight: theme("fontWeight.bold"),
+          textDecoration: "none",
+        },
+        ".BoldR-none": {
+          fontSize: theme("fontSize.R"),
+          fontWeight: theme("fontWeight.bold"),
+          textDecoration: "none",
+        },
+        ".BoldS-none": {
+          fontSize: theme("fontSize.S"),
+          fontWeight: theme("fontWeight.bold"),
+          textDecoration: "none",
+        },
+        ".MediumL-none": {
+          fontSize: theme("fontSize.L"),
+          fontWeight: theme("fontWeight.medium"),
+          textDecoration: "none",
+        },
+        ".MediumM-none": {
+          fontSize: theme("fontSize.M"),
+          fontWeight: theme("fontWeight.medium"),
+          textDecoration: "none",
+        },
+        ".MediumR-none": {
+          fontSize: theme("fontSize.R"),
+          fontWeight: theme("fontWeight.medium"),
+          textDecoration: "none",
+        },
+        ".MediumS-none": {
+          fontSize: theme("fontSize.S"),
+          fontWeight: theme("fontWeight.medium"),
+          textDecoration: "none",
+        },
+        ".MediumR-underline": {
+          fontSize: theme("fontSize.R"),
+          fontWeight: theme("fontWeight.medium"),
+          textDecoration: "underline",
+        },
+        ".MediumS-underline": {
+          fontSize: theme("fontSize.S"),
+          fontWeight: theme("fontWeight.medium"),
+          textDecoration: "underline",
+        },
+      };
+      const colorUtils = {
+        ".text-default": {
+          color: theme("grayscale.400"),
+        },
+        ".text-strong": {
+          color: theme("grayscale.500"),
+        },
+        "text-white": {
+          color: theme("grayscale.50"),
+        },
+        ".text-orange": {
+          color: theme("orange.500"),
+        },
+        ".surface-default": {
+          backgroundColor: theme("grayscale.100"),
+        },
+        ".surface-box-light": {
+          backgroundColor: theme("grayscale.50"),
+        },
+        ".surface-box-dark": {
+          backgroundColor: theme("grayscale.200"),
+        },
+        ".surface-orange": {
+          backgroundColor: theme("orange.100"),
+        },
+      };
+      addUtilities(textUtils);
+      addUtilities(colorUtils);
+    },
+  ],
 };
