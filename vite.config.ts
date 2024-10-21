d870065394de2cb95f9f65415db63d31ee111dfd
KimDayeong <dyyukim@gmail.com>
2024-10-21 14:16:35 +0900

    chore: vite.config.ts 수정

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import tsConfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsConfigPaths()],
+  build: {
+    target: "esnext", // 최신 ECMAScript 표준을 타겟으로 설정
+  },
 });
