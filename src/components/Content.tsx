366761fd5723f7a3fcf352ff7ab7021897db41ba
KimDayeong <dyyukim@gmail.com>
2024-10-23 23:25:36 +0900

    feat: content 컴포넌트 추가

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,6 +1,10 @@
-export const Content = () => (
-  <>
-    <div>this is Content!</div>
-    <div>this is Content!</div>
-  </>
-);
+import { Todo } from '@/components/Todo'
+
+export const Content = () => {
+  return (
+    <div className="displayCenter flex-col">
+      <h2 className="mt-5 BoldL-none">To Do List</h2>
+      <Todo />
+    </div>
+  )
+}
