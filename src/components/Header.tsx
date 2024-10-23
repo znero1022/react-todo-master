02c29a15179da0bbea7436cc3798e5571a49f744
KimDayeong <dyyukim@gmail.com>
2024-10-23 23:22:53 +0900

    feat: header 컴포넌트 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,18 @@
-export const Header = () => (
-  <>
-    <div>this is Header!</div>
-    <div>this is Header!</div>
-  </>
-);
+import { formatDateString, getGreeting } from '@/util'
+
+export const Header = () => {
+  const today = new Date()
+
+  return (
+    <div className="displayBetween mx-6 my-2 BoldM-none">
+      <span>
+        Hello,
+        <span className="text-orange"> Good {getGreeting(today)}!</span>
+      </span>
+      <span>
+        Time:
+        <span className="text-orange">{formatDateString(today, 'time')}</span>
+      </span>
+    </div>
+  )
+}
